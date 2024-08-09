50a4ce9da16b32b31cdd1b0a230253cbb53d4590
pravigowda18 <149652412+pravigowda18@users.noreply.github.com>
2024-08-09 17:46:43 +0530

    Sum of subset.cpp

--- subset.cpp
+++ subset.cpp
@@ -1,62 +1,45 @@
-#include <iostream>
-#include <stack>
- 
+#include <bits/stdc++.h>
 using namespace std;
- 
-int set[] = {15,5,7,9,3,4};
-int numberOfElements = 6, sum = 20;
- 
-class SubSet{
-public:
-  stack<int> solutionSet;
-  bool hasSolution;
-  
-  void solve(int s, int idx){
-      
-    if(s>sum)
-        return;
- 
-    if(s==sum){
-        hasSolution = true;
-        displaySolutionSet();
-        return;
-    }
-        
- 
-    for(int i=idx; i<numberOfElements; i++){
-        solutionSet.push(set[i]);
- 
-        solve(s+set[i],i+1);
-        
-        solutionSet.pop();
-    }
-  }
-  
-  void displaySolutionSet(){
-        stack<int> temp;
-      
-        while (!solutionSet.empty()) 
-        { 
-            cout <<  solutionSet.top() << " "; 
-            temp.push(solutionSet.top()); 
-            solutionSet.pop();
-        } 
-        cout << '\n';
-        while (!temp.empty()) 
-        { 
-            solutionSet.push(temp.top()); 
-            temp.pop();
-        } 
-    }
-};
- 
-int main()
-{
-    SubSet ss;
-    ss.solve(0,0);
-	    
-	if(ss.hasSolution == false)
-	    cout << "No Solution";
- 
-    return 0;
+
+bool flag = 0;
+void PrintSubsetSum(int i, int n, vector<int> arr, int targetSum, vector<int>& subset){
+	if (targetSum == 0) {
+	flag = 1;
+	cout << "[ ";
+	for (int i = 0; i < subset.size(); i++) {
+		cout << subset[i] << " ";
+		}
+	cout << "]" << endl;
+	return;
+	}
+	
+	if (i == n)
+		return;
+	if (arr[i] <= targetSum) 
+	{
+		subset.push_back(arr[i]);
+		PrintSubsetSum(i + 1, n, arr, targetSum - arr[i],subset);
+		subset.pop_back();
+	}
+	PrintSubsetSum(i + 1, n, arr, targetSum, subset);
+}
+
+int main(){
+	int n, data, sum;
+	vector<int> arr, subArr;
+	cout << "Enter number of elements : ";
+	cin >> n;
+	cout << "Enter " << n << " elements" << endl;
+	for(int i = 0; i < n; i++)
+	{
+		cin >> data;
+
+		arr.push_back(data); 
+	}
+	cout << "Enter a sum :";
+	cin >> sum;
+	cout << "Subset:" << endl;
+	PrintSubsetSum(0, n, arr, sum, subArr);
+	if(!flag)
+		cout << "Subset not found" << endl;
 }
